4ebadea634e40a1ed07b85691f843936941a1c16
jessekrubin <jessekrubin@gmail.com>
2020-06-24 17:12:43 -0700

    build

--- jsonbourne/__init__.py
+++ jsonbourne/__init__.py
@@ -16,23 +16,6 @@ VERSION_PATCH = 1
 VERSION_INFO = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 __version__ = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
 
-JSONS = ("rapidjson", "orjson", "ujson", "simplejson")
-JSONLIB = None
-
-
-def import_json(jsons=JSONS):
-    """Import the best possible given a priority list"""
-    for mod in jsons:
-        try:
-            return __import__(mod)
-        except ImportError:
-            pass
-    import json as _json
-
-    return _json
-
-
-json = import_json()
 JSONLIB = json.__name__
 
 __all__ = [
